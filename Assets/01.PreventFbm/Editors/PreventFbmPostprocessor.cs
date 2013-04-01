91ac2482532a3eb3fae61acd6b0111bfc1bf56b2
holykss <holy.kss@gmail.com>
2013-04-02 00:07:43 +0900

    added PreventFbmPostprocessor.cs

---
+++ Assets/01.PreventFbm/Editors/PreventFbmPostprocessor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class PreventFbmPostprocessor : AssetPostprocessor {
+
+    static void OnPostprocessAllAssets(
+        string[] importedAssets,
+        string[] deletedAssets,
+        string[] movedAssets, string[] movedFromAssetPaths)
+    {
+
+        foreach (string path in importedAssets)
+        {
+            if (path.EndsWith(".fbm"))
+            {
+                Debug.LogError("fbm 생성은 허용되지 않습니다. " + path);
+                Directory.Delete(path, true);
+                File.Delete(path + ".meta");
+
+            }
+        }
+    }
+}
